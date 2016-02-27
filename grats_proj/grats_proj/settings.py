2bf7f9b410406f86eb73a4a68d7af7124dc0fc1a
pengye <pengye91@hotmail.com>
2016-02-27 20:15:31 +0800

    modified the settings.py file

--- grats_proj/grats_proj/settings.py
+++ grats_proj/grats_proj/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from mongoengine import *
+
+DB_NAME = 'Grats'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'grats_app',
 ]
 
 MIDDLEWARE_CLASSES = [
